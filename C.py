d17dba0bf6a57bebdc5f749c4639b62da972d3eb
Alexander Mann-Wahrenberg <alexander.mannwahrenberg@gmail.com>
2022-03-03 15:37:24 +0100

    Constant function

--- C.py
+++ C.py
@@ -4,21 +4,20 @@ import matplotlib.pyplot as plt
 
 # function that returns dy/dt
 def model(y,t):
-    k = 0.3
-    dydt = -k * y
+    dydt = 0
     return dydt
 
 # initial condition
-y0 = 5
+y0 = 1
 
 # time points
-t = np.linspace(0,20)
+t = np.linspace(0,20, 5)
 
 # solve ODE
 y = odeint(model,y0,t)
 
 # plot results
-plt.plot(t,y)
+plt.plot(t,y, '.')
 plt.xlabel('time')
 plt.ylabel('y(t)')
 plt.show()
